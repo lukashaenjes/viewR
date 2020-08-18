656091df0babe8263934bf693c0adae9288839e3
lukasfeick-sw <lukas.feick@statworx.com>
2020-08-18 17:46:32 +0200

    Add roxygen tags and clean

--- R/view_selection.R
+++ R/view_selection.R
@@ -1,79 +1,50 @@
-view_selection_tidy <- function(){
-
-  context <- rstudioapi::getSourceEditorContext()
-
-  selection <- base::trimws(context$selection[[1]]$text)
-
-  title <- paste0(stringr::str_extract(string = selection,
-                                       #pattern = "([^\\s]+)"),
-                                       # pattern = "[^ %]*"),
-                                       pattern = ".+(?= %)"),
-                  "_view")
-
-  rstudioapi::sendToConsole(
-    glue::glue(selection,
-               " %>%\n  View(title = \"{title}\")")
-  )
-
-}
+#' View selected lines in RStudio data viewer
+#'
+#' @importFrom rstudioapi getSourceEditorContext sendToConsole
+#' @importFrom magrittr %>%
+#'
+#' @export
+#'
 
 view_selection <- function(){
 
-  context <- rstudioapi::getSourceEditorContext()
+  # capture context
+  context <- getSourceEditorContext()
 
-  selection <- base::trimws(context$selection[[1]]$text)
+  # extract selected lines, removing whitespace
+  selection <- trimws(context$selection[[1]]$text)
 
-  title <- base::unlist(
-    base::regmatches(selection,
-                     base::regexec(".+(?= %)",
-                                   selection,
-                                   perl = TRUE)
+  # remove everything after the last closing bracket
+  selection <- unlist(
+    regmatches(selection,
+               regexec("[^)]+$",
+                       selection,
+                       perl = TRUE),
+               invert = TRUE
     )
-  )
+  )[1]
 
-  title <- base::paste0(title, "_view")
+  # extract the title: everything before the first '%' (start of the pipe)
+  title <- trimws(unlist(
+    regmatches(selection,
+               regexec("^[^%]+",
+                       selection,
+                       perl = TRUE)
+    )
+  ))
 
-  rstudioapi::sendToConsole(
-    base::paste0(selection,
-                 " %>%\n  View(title = '", title,"')")
+  # append title
+  title <- paste0(title, "_view")
+
+  # paste `View` call and send to console
+  sendToConsole(
+    paste0(selection,
+           " %>%\n  View(title = '", title,"')")
   )
 
 }
 
-
-# TODO:
-# match title bis erstes % statt bis ersten space
-# take base View with "title"
-# if pipe is at the end then remove
-# dplyr is used, however since this package is intended for use with dplyr the
-#   piping made sense -- usethis::usepipe
-# take out deps
-
-
-#
-#
-# x <- rstudioapi::getSourceEditorContext()
-#
-# x$selection[[1]]$text
-#
-# rstudioapi::sendToConsole("asdf <- 5")
-#
-#
 # test <- mtcars %>%
 #   dplyr::bind_rows(mtcars) %>%
 #   dplyr::bind_rows(mtcars) %>%
 #   tibble::as_tibble()
-#
-# rstudioapi::sendToConsole(paste0(x$selection[[1]]$text, " %>%\n  View()"))
-#
-# title <- paste0(stringr::str_extract(string = x$selection[[1]]$text,
-#                                      pattern = "([^\\s]+)"), "_view")
-#
-# rstudioapi::sendToConsole(glue::glue(x$selection[[1]]$text, " %>%\n  tibble::view(title = \"{title}\")"))
-#
-# test %>%
-#   dplyr::filter(mpg < 20) %>%
-#   tibble::view(title = "asdf")
-#
-# glue::glue("This is {obj}", obj = "asdf")
-# #
